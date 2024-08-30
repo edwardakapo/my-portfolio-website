6d825122641a8d9eac086e3c6e0b425f43028bc8
Edward Akapo <edwardakapo@outlook.com>
2024-08-29 21:57:00 -0400

    spacing fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 export default function Home() {
   
   return (
-  <section  className='HomePage flex gap-x-12  max-w-3xl mb-10 px-5'>
+  <section  className='HomePage flex gap-x-12  max-w-[810px] items-center mb-10 px-5'>
       <div className='max-w-[26rem] HomePicture'>
         <PagePhoto desc="myphotosssss" src="/dinner_pic.jpg"/>
       </div>
@@ -16,7 +16,7 @@ export default function Home() {
         <br/>
         <p className='text-sm'>Specializing in scalable web applications, my expertise includes front and back-end technologies, cloud architecture, and advanced API development. I ensure robust deployment pipelines, well-tested code, and responsive, functional UI/UX.</p>
         <br/>
-        <p className='text-sm'>As a freelance developer, I offer custom solutions to small businesses, allowing them to streamline operations, optimize workflows, and enhance their overall efficiency."</p>
+        <p className='text-sm'>As a freelance developer, I offer custom solutions to small businesses, allowing them to streamline operations, optimize workflows, and enhance their overall efficiency.</p>
         <br/>
         <p className='text-sm'>You can find links to my more recent projects <Link href="/Projects"><span className='text-sm font-bold inline hover:text-stone-400'>here,</span></Link>  or visit my <Link href="https://github.com/edwardakapo" target='_blank'><span className='text-sm font-bold inline hover:text-stone-400'>Github</span></Link> to get a look at all my projects</p>
         <br/>
